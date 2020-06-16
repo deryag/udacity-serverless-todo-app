a517486abda5f25c2d94ca59dc2cda77cd90c6ed
TCDGOK <derya.gok@turkcell.com.tr>
2020-06-16 12:48:34 +0300

    Auth settings image added

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,9 +1,9 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '3abhe74ta';
+const apiId = 'h3abhe74ta';
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`;
 
 export const authConfig = {
   domain: 'deryag.auth0.com',            // Auth0 domain
-  clientId: '3cccOX58IWHRPxGQWbZSpfDJ6ajvjmAC',          // Auth0 client id
+  clientId: '03N67jeIm9f8IC77oQnJ9rDQGvo9uYsJ',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 };
